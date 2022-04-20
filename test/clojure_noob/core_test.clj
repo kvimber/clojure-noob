a259667495b4ef8f6978fca8e81761900427c263
kvimber <kevin.imber@circleci.com>
2022-04-19 18:13:19 -0700

    plug template testing, so it doesn't fail always

--- test/clojure_noob/core_test.clj
+++ test/clojure_noob/core_test.clj
@@ -3,5 +3,5 @@
             [clojure-noob.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "FIXME no longer required, I've been rendered useless."
+    (is (= 1 1))))
